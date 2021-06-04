df30e372e7e10131f8fddfed09e9705b3ad32d27
github_julienmcoding <manois.julien@gmail.com>
2021-06-04 08:08:59 +0200

    change route test

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 module.exports = apiRouter;
 
 // create a new user
-apiRouter.post('/users', bodyParser.urlencoded({ extended: false }), async (req, res) => {
+apiRouter.post('/users/new-user', bodyParser.urlencoded({ extended: false }), async (req, res) => {
     const { username } = req.body.username;
     try {
         await pool.query('INSERT INTO users (username, _id) VALUES ($1, uuid_generate_v4())', [username]);
